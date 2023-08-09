4baac43f3c6807811a0f7c0696e20c9905cbd062
Peng Boris <peng@traleor.com>
2023-08-09 03:33:23 +0100

    setup fonts

---
+++ lib/font.ts
@@ -0,0 +1,61 @@
+import { Poppins, Montserrat, Merriweather } from "next/font/google";
+
+export const secondary = Montserrat({
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-1",
+  fallback: [
+    "-apple-system",
+    "Segoe UI",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "Fira Sans",
+    "Droid Sans",
+    "Helvetica Neue",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+});
+
+export const primary = Poppins({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-2",
+  fallback: [
+    "-apple-system",
+    "Segoe UI",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "Fira Sans",
+    "Droid Sans",
+    "Helvetica Neue",
+    "Helvetica",
+    "sans-serif",
+  ],
+});
+
+export const tertiary = Merriweather({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["300", "400", "700", "900"],
+  variable: "--font-3",
+  fallback: [
+    "-apple-system",
+    "Segoe UI",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "Fira Sans",
+    "Droid Sans",
+    "Helvetica Neue",
+    "Helvetica",
+    "sans-serif",
+  ],
+});
